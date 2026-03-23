cbb6cbd8aa81a098d4556ba9da6b424de6eaef28
AmitKumar-AK <amitkumar_mca04@hotmail.com>
2026-03-23 12:11:50 +0000

    initial tool changes.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using McpSitecoreAiExample;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<MonkeyService>();
+builder.Services.AddSingleton<SellingService>();
+builder.Services.AddSingleton<SitecoreAIService>();
+
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
